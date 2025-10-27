be5ac033a0ae05928608e04b3db8491ac00a4010
Dmitry <andrewgamer63@gmail.com>
2025-10-28 00:03:31 +0300

    constraints UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { main } from "./simplex/utils";
 import TopBarImg from "./assets/topbar_2.png"
 import Fraction from "fraction.js";
+import { Matrix } from "./simplex/types";
 
 function App() {
   const [error, setError] = useState("");
+  const [isArtificialBasis, setIsArtificialBasis] = useState(false);
   const [problemSize, setProblemSize] = useState(2);
   const [constraintsSize, setConstraintsSize] = useState(2);
-  
+
   const [fnCoeffs, setFnCoeffs] = useState<Fraction[]>([]);
+  const [constraints, setConstraints] = useState<Matrix>([]);
 
   useEffect(() => {
     // main()
@@ -38,44 +40,139 @@ function App() {
     }
   }
 
+  const handleChangeConstraints = (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      const nums = []
+      for (let i = 0; i < constraintsSize; i++) {
+        nums.push(new Fraction(e.target.form[i].value))
+      }
+      setConstraints(nums)
+      console.log(nums);
+    } catch (e) {
+      console.error("Неверное выражение")
+    }
+  }
+
   return (
     <main className="container">
       <div className="topbar">
-        <img src={TopBarImg} alt="" loading="lazy"/>
+        <img src={TopBarImg} alt="" loading="lazy" />
       </div>
       <div className="size-input">
         <label htmlFor="problem-size">Количество переменных: </label>
-        <input value={problemSize} onChange={handleChangeProblemSize} type="number" id="problem-size" name="problem-size" min="2" max="16" defaultValue="2" />
+        <input
+          value={problemSize}
+          onChange={handleChangeProblemSize}
+          type="number"
+          id="problem-size"
+          name="problem-size"
+          min="2"
+          max="16"
+          defaultValue="2"
+        />
 
         <label htmlFor="constraints-size">Количество ограничений: </label>
-        <input value={constraintsSize} onChange={handleChangeConstraintsSize} type="number" id="constraints-size" name="constraints-size" min="2" max="16" defaultValue="2" />
+        <input
+          value={constraintsSize}
+          onChange={handleChangeConstraintsSize}
+          type="number"
+          id="constraints-size"
+          name="constraints-size"
+          min="2"
+          max="16"
+          defaultValue="2"
+        />
       </div>
       <hr />
       <form className="function-input" onChange={handleChangeFunction}>
         <label htmlFor="function">f(x): </label>
-        {Array(problemSize).fill(0).map((_, idx) => 
-        <>
-          <input key={idx} type="string" id={`x${idx + 1}`} name={`x${idx + 1}`} />
-          <span> x{idx + 1}</span>
-        </>
-        ).reduce((prev, curr) => [prev, <span> + </span>, curr])}
-        <input type="string" id="free-coeff" name="free-coeff" defaultValue="0" />
+        {Array(problemSize)
+          .fill(0)
+          .map((_, idx) => (
+            <>
+              <input
+                key={idx}
+                type="string"
+                id={`x${idx + 1}`}
+                name={`x${idx + 1}`}
+                defaultValue={1}
+              />
+              <span> x{idx + 1}</span>
+            </>
+          ))
+          .reduce((prev, curr) => [prev, <span> + </span>, curr])}
+        <span> = </span>
+        <input
+          type="string"
+          id="free-coeff"
+          name="free-coeff"
+          defaultValue={0}
+        />
       </form>
-      <h1>Welcome to Tauri + React</h1>
+      <form className="constraints-input" onChange={handleChangeConstraints}>
+        <label htmlFor="constraints">Ограничения: </label>
+        <br />
+        {Array(constraintsSize)
+          .fill(0)
+          .map((row, i) => {
+            return (
+              <>
+                {Array(problemSize)
+                  .fill(0)
+                  .map((_, idx) => (
+                    <>
+                      <input
+                        key={idx}
+                        type="string"
+                        id={`x${idx + 1}`}
+                        name={`x${idx + 1}`}
+                        defaultValue={1}
+                      />
+                      <span> x{idx + 1}</span>
+                    </>
+                  ))
+                  .reduce((prev, curr) => [prev, <span> + </span>, curr])}
+                <span> = </span>
+                <input
+                  type="string"
+                  id="free-coeff"
+                  name="free-coeff"
+                  defaultValue={0}
+                />
+                <br />
+              </>
+            );
+          })}
+      </form>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>
+        <label htmlFor="isArtificialBasis">Искусственный базис: </label>
+        <input
+          onChange={(e) => setIsArtificialBasis(e.target.checked)}
+          checked={isArtificialBasis}
+          type="checkbox"
+          name="isArtificialBasis"
+          id="isArtificialBasis"
+        />
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
+      <form className="basis-input">
+        <label htmlFor="basis">Базис: </label>
+        {
+          Array(problemSize).fill(0).map((_, idx) => (
+            <>
+              <input
+                key={idx}
+                type="checkbox"
+                id={`basis-x${idx + 1}`}
+                name={`x${idx + 1}`}
+                defaultValue={1}
+              />
+              <label htmlFor={`basis-x${idx + 1}`} className="basis-x"> x{idx + 1}</label>
+            </>
+          ))
+        }
+      </form>
     </main>
   );
 }
